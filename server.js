136245c08e8e20ba937e79714a6acb6ed513f16a
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-05-17 07:56:05 -0400

    99 bottles of beer attempt...

--- server.js
+++ server.js
@@ -25,12 +25,17 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
 
 let eightBallArray =["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
-app.get('/magic/:millionaireQuestion', (req, res)=>{
- let millionaireQuestion = `Will I Be A Millionaire`
+// app.get('/magic/:millionaireQuestion', (req, res)=>{
+//  let millionaireQuestion = `Will I Be A Millionaire`
        
-            res.send(`${millionaireQuestion, <h1>Math.random(eightBallArray).floor</h1>}`)
-        })
+//             res.send(`${millionaireQuestion, <h1>Math.random(eightBallArray).floor</h1>}`)
+//         })
         
+ // Make a route '/greeting'that sends a generic greeting to the screen like "Hello, stranger"
+app.get('/', (req, res)=>{
+    res.send(`99 Bottles of beer on the wall, ${<a href = "">"take one down, pass it around"</a> }`)
+   
+})
     
 
 
