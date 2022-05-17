c14e5a4fb34a8f589daac7816c9a74c049ad823b
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-05-17 07:40:35 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -22,4 +22,16 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
  
      res.send(`${req.params.total * (req.params.tipPercentage * .01)}`)
 })
+
+let eightBallArray =["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:millionaireQuestion', (req, res)=>{
+ let millionaireQuestion = `Will I Be A Millionaire`
+       
+            res.send(`${millionaireQuestion, <h1>Math.random(eightBallArray).floor</h1>}`)
+        })
+        
+    
+
+
    
